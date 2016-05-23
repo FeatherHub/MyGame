09e5815869086ded2a9dac986680fd1d267e3280
FeatherHub <woook95@naver.com>
2016-05-23 15:46:43 +0900

    Add Cellphone game scene

---
+++ Classes/CellPhoneGameScene.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "CellPhoneGameScene.h"
+
+bool CellPhoneGameScene::init()
+{
+	if (Layer::init() == false)
+		return false;
+
+	return true;
+}
---
+++ Classes/CellPhoneGameScene.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class CellPhoneGameScene : public Layer
+{
+public:
+	static Scene* createScene();
+	virtual bool init();
+	CREATE_FUNC(CellPhoneGameScene);
+
+private:
+
+};
